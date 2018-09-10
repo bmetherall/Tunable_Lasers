3064d5a2e03efbf0382a72f9a3c23f1228374d4d
Brady Metherall <brady.metherall@uoit.net>
2018-09-10 11:11:14 -0400

    Generated squish data

--- Lasers.py
+++ Lasers.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.special.lambertw as W
 import matplotlib.pyplot as plt
+import time
 
 def Energy(A, dx):
 	return np.real(np.sum(A * np.conj(A)) * dx)
@@ -17,7 +18,9 @@ def Loss(A, h = 0.1):
 	return h * A
 
 def Mod(A, T, nu = 1, mu = 1):
-	return 1.0 / 2.0 * (1 - nu * np.cos(mu * np.pi * np.exp(-T**2))) * A
+	return np.exp(-T**2 / 2) * A
+	#return 1.0 / 2.0 * (1 - nu * np.cos(mu * np.pi * np.exp(-T**2))) * A
+	#return 1.0 / 2.0 * (np.cos(np.pi * T) + 1) * (T < 1) * (T > -1) * A
 
 def Fibre(A, f = 10):
 	return np.exp(1j * f * A * np.conj(A)) * A
@@ -29,15 +32,15 @@ def Disp(A, T, s = 0.1):
 	return np.fft.ifft(F * np.exp(1j * w**2 * s**2))
 
 # 1 round trip
-def Loop(A, T, dx):
-	A = Gain(A, Energy(A, dx))
-	A = Disp(A, T)
+def Loop(A, T, dx, s):
+	#A = Gain(A, Energy(A, dx))
+	#A = Fibre(A, 0)
+	#A = Loss(A, 0.5)
+	A = Disp(A, T, s)
 	A = Mod(A, T)
-	A = Loss(A, 0.25)
-	A = Fibre(A)
 	return A
 
-N = 280 # Number of loops of the circuit
+N = 100 # Number of loops of the circuit
 p = 2**12 # Number of points in the discretization
 width = 4 # Size of window
 E0 = 1 # Initial energy
@@ -45,12 +48,12 @@ E0 = 1 # Initial energy
 # Initialization
 T = np.linspace(-width, width, p, endpoint = False)
 dx = T[1] - T[0]
-A0 = np.exp(1j * np.pi * 1.0 / 4.0) / np.cosh(5 * T)
+A0 = 1 / np.cosh(2 * T) * np.exp(1j * np.pi / 4)
 A0 = np.sqrt(E0 / Energy(A0, dx)) * A0 # Normalize
 E = np.zeros(N)
 data = np.zeros((2 * N, p))
 A = A0
-
+'''
 # Things for animated plot
 plt.ion()
 fig = plt.figure()
@@ -59,38 +62,75 @@ line1, = ax.plot(T, np.real(A), 'r-', label = 'Real')
 line2, = ax.plot(T, np.imag(A), 'b-', label = 'Imaginary')
 line3, = ax.plot(T, np.abs(A), 'g-', label = 'Magnitude')
 
+fig.canvas.draw()
+fig.canvas.flush_events()
+
 plt.legend()
 plt.xlim(-2, 2)
-plt.ylim(-0.25, 0.25)
+plt.ylim(-1, 1)
 
 # N round trips of the laser
 for i in range(N):
-	A = Loop(A, T, dx)
-	data[2*i] = np.real(A)
-	data[2*i+1] = np.imag(A)
-	E[i] = Energy(A, dx)
-
 	# Animate the plot
 	line1.set_ydata(np.real(A))
 	line2.set_ydata(np.imag(A))
 	line3.set_ydata(np.abs(A))
 	fig.canvas.draw()
 	fig.canvas.flush_events()
+	#time.sleep(2)
 
-	#if i == 40:
-	#	plt.close()
-	#	plt.plot(np.fft.fft(A))
-	#	plt.show()
-	#	plt.pause(20)
+	A = Loop(A, T, dx, 0.2)
+	data[2*i] = np.real(A)
+	data[2*i+1] = np.imag(A)
+	E[i] = Energy(A, dx)
+	print E[i]
+
+steps = np.zeros((12, p))
+
+steps[0] = np.real(A)
+steps[1] = np.imag(A)
+A = Gain(A, Energy(A, dx))
+steps[2] = np.real(A)
+steps[3] = np.imag(A)
+A = Fibre(A, 0.5)
+steps[4] = np.real(A)
+steps[5] = np.imag(A)
+A = Loss(A, 0.5)
+steps[6] = np.real(A)
+steps[7] = np.imag(A)
+A = Disp(A, T)
+steps[8] = np.real(A)
+steps[9] = np.imag(A)
+A = Mod(A, T)
+steps[10] = np.real(A)
+steps[11] = np.imag(A)
+
+np.savetxt('Steps.dat', np.vstack((T, steps)).T)
+
 
 #np.savetxt('Steady.dat', (np.vstack((T, data[-2:], data[-2]**2 + data[-1]**2))).T)
 
-plt.close()
+'''
 
-plt.plot(E)
-plt.show()
+n = 200
+data = np.zeros(n)
+s = np.linspace(0, 0.8, num = n)
 
-plt.pause(4)
+for j in range(n):
+	A0 = 1 / np.cosh(2 * T) * np.exp(1j * np.pi / 4)
+	A0 = np.sqrt(E0 / Energy(A0, dx)) * A0 # Normalize
+	A = A0
+	for i in range(100):
+		old = np.abs(A[len(A)/2])
+		A = Loop(A, T, dx, s[j])
+		new = np.abs(A[len(A)/2])
+		data[j] = new / old
+
+
+np.savetxt('sdata.dat', np.vstack((s, data)).T)
+#plt.plot(s, data)
+#plt.show()
+
 
 
 
