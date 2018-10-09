934e10d1c4699533fac6598c5735d964a1d174aa
bmetherall <brady.metherall@uoit.net>
2018-10-09 12:29:12 -0400

    Added Sean's additions

--- Lasers.py
+++ Lasers.py
@@ -4,46 +4,49 @@ import matplotlib.pyplot as plt
 import time
 
 def Energy(A, dx):
-	return np.real(np.sum(A * np.conj(A)) * dx)
+	return np.trapz(np.real(A * np.conj(A)), dx = dx)
+
+def zeta(s):
+	return np.exp(-0.511444 * s + 0.00174128)
+
+def EE(a, s, h):
+	r = zeta(s)**2 * h**2
+	return r / (1 - r) * np.log(a * r)
 
 # Functions for each component
 def Gain(A, E, a = 30):
-	if E < 20:
-		return np.sqrt(W(a * E * np.exp(E)) / E) * A
-	else:
-		# For large E python has overflow problems, but this approximation can be used instead
-		return np.sqrt((E + np.log(a)) / E) * A
+	return np.real(np.sqrt(W(a * E * np.exp(E)) / E)) * A
 
-def Loss(A, h = 0.1):
+def Loss(A, h = 0.4):
 	return h * A
 
-def Mod(A, T, nu = 1, mu = 1):
+def Mod(A, T):
 	return np.exp(-T**2 / 2) * A
-	#return 1.0 / 2.0 * (1 - nu * np.cos(mu * np.pi * np.exp(-T**2))) * A
-	#return 1.0 / 2.0 * (np.cos(np.pi * T) + 1) * (T < 1) * (T > -1) * A
 
-def Fibre(A, f = 10):
-	return np.exp(1j * f * A * np.conj(A)) * A
+def Fibre(A, b = 0.1):
+	return np.exp(1j * b * np.real(np.abs(A)**2)) * A
 
 def Disp(A, T, s = 0.1):
 	F = np.fft.fft(A)
+	F = (np.abs(F) > 10**-4) * F
 	dw = np.pi / T[-1]
 	w = np.fft.fftfreq(len(A)) * len(A) * dw
-	return np.fft.ifft(F * np.exp(1j * w**2 * s**2))
+	return np.fft.ifft(F * np.exp(1j * (w**2 * s**2 - 0 * s**2 * w**3)))
 
 # 1 round trip
-def Loop(A, T, dx, s):
-	#A = Gain(A, Energy(A, dx))
-	#A = Fibre(A, 0)
-	#A = Loss(A, 0.5)
+def Loop(A0, T, dx, s, b):
+	A = A0
+	A = Gain(A0, Energy(A0, dx))
+	A = Fibre(A, b)
+	A = Loss(A, 0.6)
 	A = Disp(A, T, s)
 	A = Mod(A, T)
 	return A
 
 N = 100 # Number of loops of the circuit
-p = 2**12 # Number of points in the discretization
-width = 4 # Size of window
-E0 = 1 # Initial energy
+p = 2**14 # Number of points in the discretization
+width = 64 # Size of window
+E0 = 0.1 # Initial energy
 
 # Initialization
 T = np.linspace(-width, width, p, endpoint = False)
@@ -53,7 +56,12 @@ A0 = np.sqrt(E0 / Energy(A0, dx)) * A0 # Normalize
 E = np.zeros(N)
 data = np.zeros((2 * N, p))
 A = A0
-'''
+
+#######################################
+
+dw = np.pi / T[-1]
+w = np.fft.fftfreq(len(A)) * len(A) * dw
+
 # Things for animated plot
 plt.ion()
 fig = plt.figure()
@@ -66,72 +74,57 @@ fig.canvas.draw()
 fig.canvas.flush_events()
 
 plt.legend()
-plt.xlim(-2, 2)
-plt.ylim(-1, 1)
+#plt.xlim(-5000, 5000)
+#plt.ylim(0, 0.02)
+
+plt.xlim(-4, 4)
+plt.ylim(-0.5, 0.5)
 
 # N round trips of the laser
 for i in range(N):
 	# Animate the plot
+	#line1.set_ydata(np.real(np.fft.fft(A)))
+	#line2.set_ydata(np.imag(np.fft.fft(A)))
+	line3.set_ydata(np.abs(np.fft.fft(A)))
+
 	line1.set_ydata(np.real(A))
 	line2.set_ydata(np.imag(A))
 	line3.set_ydata(np.abs(A))
 	fig.canvas.draw()
 	fig.canvas.flush_events()
-	#time.sleep(2)
+	#time.sleep(0.25)
+	print i
 
-	A = Loop(A, T, dx, 0.2)
-	data[2*i] = np.real(A)
-	data[2*i+1] = np.imag(A)
+	A = Loop(A, T, dx, 0.4, 100)
 	E[i] = Energy(A, dx)
-	print E[i]
 
-steps = np.zeros((12, p))
+np.savetxt('E.dat', E)
 
-steps[0] = np.real(A)
-steps[1] = np.imag(A)
-A = Gain(A, Energy(A, dx))
-steps[2] = np.real(A)
-steps[3] = np.imag(A)
-A = Fibre(A, 0.5)
-steps[4] = np.real(A)
-steps[5] = np.imag(A)
-A = Loss(A, 0.5)
-steps[6] = np.real(A)
-steps[7] = np.imag(A)
-A = Disp(A, T)
-steps[8] = np.real(A)
-steps[9] = np.imag(A)
-A = Mod(A, T)
-steps[10] = np.real(A)
-steps[11] = np.imag(A)
+np.savetxt('Envelope.dat', np.vstack((T, np.real(A), np.imag(A), np.abs(A))).T)
 
-np.savetxt('Steps.dat', np.vstack((T, steps)).T)
+############################################
+'''
+n = 51
+z = np.zeros((n**2, 4))
+s = np.linspace(0, 1.6, num = n)
+b = np.linspace(0, 5, num = n)3
+count = 0
 
+for k in range(n):
+	for j in range(n):
+		A0 = 1 / np.cosh(2 * T) * np.exp(1j * np.pi / 4)
+		#A0 = np.exp(-T**2 / 2)
+		A0 = np.sqrt(E0 / Energy(A0, dx)) * A0 # Normalize
+		A = A0
+		for i in range(50):
+			old = np.abs(A)
+			A = Loop(A, T, dx, s[j], b[k])
+			new = np.abs(A)
+		z[count] = s[j], b[k], np.sqrt(np.trapz((old - new)**2, dx = dx)) / np.sqrt(np.trapz(old**2, dx = dx)), Energy(A, dx)
+		count += 1
 
-#np.savetxt('Steady.dat', (np.vstack((T, data[-2:], data[-2]**2 + data[-1]**2))).T)
+#np.savetxt('Zeta.dat', np.vstack((s, z)).T)
+np.savetxt('a30.dat', z)
 
 '''
-
-n = 200
-data = np.zeros(n)
-s = np.linspace(0, 0.8, num = n)
-
-for j in range(n):
-	A0 = 1 / np.cosh(2 * T) * np.exp(1j * np.pi / 4)
-	A0 = np.sqrt(E0 / Energy(A0, dx)) * A0 # Normalize
-	A = A0
-	for i in range(100):
-		old = np.abs(A[len(A)/2])
-		A = Loop(A, T, dx, s[j])
-		new = np.abs(A[len(A)/2])
-		data[j] = new / old
-
-
-np.savetxt('sdata.dat', np.vstack((s, data)).T)
-#plt.plot(s, data)
-#plt.show()
-
-
-
-
 
