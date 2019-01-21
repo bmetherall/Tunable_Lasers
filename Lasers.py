bcb47516c85d33c5cae493c9cc2f34fd72f08f08
bmetherall <brady.metherall@uoit.net>
2019-01-21 11:41:13 -0500

    Cleaned up file quite a bit

--- Lasers.py
+++ Lasers.py
@@ -1,17 +1,29 @@
+##################################################
+#			Brady Metherall
+#			MSc Thesis
+##################################################
+'''
+This is the code used for my MSc thesis. This code
+is separated into four parts each described below.
+'''
+
+###############
+#	Part I
+###############
+'''
+Function definitions and initialization
+'''
+
 import numpy as np
 import scipy.special.lambertw as W
 import matplotlib.pyplot as plt
-import time
+from scipy.optimize import curve_fit
 
+def func(x, a, b):
+	return a * np.exp(-x**2 / (2 * b**2))
+ 
 def Energy(A, dx):
 	return np.trapz(np.real(A * np.conj(A)), dx = dx)
-
-def zeta(s):
-	return np.exp(-0.511444 * s + 0.00174128)
-
-def EE(a, s, h):
-	r = zeta(s)**2 * h**2
-	return r / (1 - r) * np.log(a * r)
 
 # Functions for each component
 def Gain(A, E, a = 8000):
@@ -23,119 +35,152 @@ def Loss(A, h = 0.04):
 def Mod(A, T):
 	return np.exp(-T**2 / 2) * A
 
-def Fibre(A, b = 0.25):
-	return np.exp(1j * b * np.real(np.abs(A)**2)) * A
+def Fibre(A, b = 1.0):
+	return np.exp(1j * b * np.abs(A)**2) * A
 
-def Disp(A, T, s = 0.2):
+def Disp(A, T, s = 0.1):
 	F = np.fft.fft(A)
-	F = (np.abs(F) > 10**-4) * F
+	F = F * (np.abs(F) > 10**-4) # Numerical stability
 	dw = np.pi / T[-1]
 	w = np.fft.fftfreq(len(A)) * len(A) * dw
-	return np.fft.ifft(F * np.exp(1j * (w**2 * s**2 - 0 * s**2 * w**3)))
+	return np.fft.ifft(F * np.exp(1j * w**2 * s**2)))
 
 # 1 round trip
-def Loop(A0, T, dx, s, b):
-	A = A0
-	A = Gain(A0, Energy(A0, dx))
-	#A = Fibre(A, b)
+def Loop(A, T, dx, s, b, switch = False):
 	A = Loss(A)
-	#old = np.gradient(-np.gradient(np.angle(A), dx),dx)[len(A)/2]
-	A = Disp(A, T, s)
-	#new = np.gradient(-np.gradient(np.angle(A), dx),dx)[len(A)/2]
-	A = Mod(A, T)
-	#print old - new
+	if not switch:
+		A = Disp(A, T, s)
+		A = Mod(A, T)
+	else:
+		A = Mod(A, T)
+		A = Disp(A, T, s)
+	A = Gain(A, Energy(A, dx))
+	A = Fibre(A, b)
 	return A
 
-N = 50 # Number of loops of the circuit
-p = 2**14 # Number of points in the discretization
-width = 128 # Size of window
-E0 = 0.0001 # Initial energy
+N = 250 # Number of loops of the circuit
+p = 2**12 # Number of points in the discretization
+width = 64 # Size of window
+E0 = 0.1 # Initial energy
 
 # Initialization
 T = np.linspace(-width, width, p, endpoint = False)
 dx = T[1] - T[0]
-#A0 = 1 / np.cosh(2 * T) * np.exp(1j * np.pi / 4)
-A0 = np.exp(-(1+2.89005*1j)*T**2 / (2*0.938229**2))
+A0 = 1 / np.cosh(2 * T) * np.exp(1j * np.pi / 4)
 A0 = np.sqrt(E0 / Energy(A0, dx)) * A0 # Normalize
 E = np.zeros(N)
 data = np.zeros((2 * N, p))
 A = A0
 
-#######################################
+part = 4 # Select which part of the code to run
 
-dw = np.pi / T[-1]
-w = np.fft.fftfreq(len(A)) * len(A) * dw
+if part == 2:
+	###############
+	#	Part II
+	###############
+	'''
+	On the fly animation of single realizations
+	'''
+	plt.ion()
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	line1, = ax.plot(T, np.real(A), 'r-', label = 'Real')
+	line2, = ax.plot(T, np.imag(A), 'b-', label = 'Imaginary')
+	line3, = ax.plot(T, np.abs(A), 'g-', label = 'Magnitude')
 
-# Things for animated plot
-plt.ion()
-fig = plt.figure()
-ax = fig.add_subplot(111)
-#line1, = ax.plot(T, np.real(A), 'r-', label = 'Real')
-#line2, = ax.plot(T, np.imag(A), 'b-', label = 'Imaginary')
-line3, = ax.plot(T, np.angle(A), 'g-', label = 'Magnitude')
-
-fig.canvas.draw()
-fig.canvas.flush_events()
-
-plt.legend()
-#plt.xlim(-5000, 5000)
-#plt.ylim(0, 0.02)
-
-plt.xlim(-4, 4)
-plt.ylim(-10, 10)
-
-dw = np.pi / T[-1]
-w = np.fft.fftfreq(len(A)) * len(A) * dw
-
-# N round trips of the laser
-for i in range(N):
-	# Animate the plot
-	#line1.set_ydata(np.real(np.fft.fft(A)))
-	#line2.set_ydata(np.imag(np.fft.fft(A)))
-	#line3.set_ydata(np.abs(np.fft.fft(A)))
-
-	#line1.set_ydata(np.real(A))
-	#line2.set_ydata(np.imag(A))
-	line3.set_ydata(np.gradient(-np.gradient(np.angle(A), dx), dx))
 	fig.canvas.draw()
 	fig.canvas.flush_events()
-	time.sleep(1)
-	#print np.gradient(-np.gradient(np.angle(A), dx), dx)[len(A)/2]
 
-	A = Loop(A, T, dx, 1.0, 0)
-	E[i] = Energy(A, dx)
+	plt.legend()
+	plt.xlim(-2, 2)
+	plt.ylim(-1, 1)
 
-	#np.savetxt('Envelope' + str(i) + '.dat', np.vstack((T, np.real(A), np.imag(A), np.abs(A), np.angle(A), w, np.abs(np.fft.fft(A)))).T)
+	# N round trips of the laser
+	for i in range(N):
+		# Animate the plot
+		line1.set_ydata(np.real(A))
+		line2.set_ydata(np.imag(A))
+		line3.set_ydata(np.abs(A))
+		fig.canvas.draw()
+		fig.canvas.flush_events()
+		#time.sleep(2)
+		print i
 
-np.savetxt('E.dat', E)
+		A = Loop(A, T, dx, 0.41, 2)
+		E[i] = Energy(A, dx)
+		#time.sleep(2)
 
-np.savetxt('Envelope.dat', np.vstack((T, np.real(A), np.imag(A), np.abs(A))).T)
-print np.abs(A[len(A)/2])
-print np.std(np.abs(A))
+	#np.savetxt('E.dat', E)
 
-############################################
-'''
-n = 51
-z = np.zeros((n**2, 4))
-s = np.linspace(0, 1.6, num = n)
-b = np.linspace(0, 5, num = n)3
-count = 0
+	#np.savetxt('Envelope.dat', np.vstack((T, np.abs(A))).T)
 
-for k in range(n):
+elif part == 3:
+	###############
+	#	Part III
+	###############
+	'''
+	Run the simulation for an nxn grid in s-b space
+	'''
+	n = 201
+	#z = np.zeros((n**2, 4))
+	zoom = False
+
+	if zoom:
+		s = np.linspace(0, 0.25, num = n)
+		#b = np.logspace(3.0, 5.0, num = n)
+		b = np.linspace(0, 3, num = n)
+	else:
+		s = np.linspace(0, 1, num = n)
+		#b = np.logspace(3.0, 6.0, num = n)
+		b = np.linspace(0, 50, num = n)
+
+	filename = '8000-04-01-DM.dat'
+	open(filename, 'w').close()
+	f = open(filename, 'ab')
+
+	for k in range(n):
+		print k
+		z = np.zeros((n, 4))
+		for j in range(n):
+			A0 = 1 / np.cosh(2 * T) * np.exp(1j * np.pi / 4)
+			A0 = np.sqrt(E0 / Energy(A0, dx)) * A0 # Normalize
+			A = A0
+			for i in range(25):
+				old = np.abs(A)
+				A = Loop(A, T, dx, s[j], b[k], switch = False)
+				new = np.abs(A)
+			z[j] = s[j], b[k], np.sqrt(np.trapz((old - new)**2, dx = dx)) / np.sqrt(np.trapz(old**2, dx = dx)), Energy(A, dx)
+		np.savetxt(f, z)
+		f.write('\n')
+
+	f.close()
+
+elif part == 4:
+	###############
+	#	Part IV
+	###############
+	'''
+	Compute features of linear model (E, P, sigma, C, phi)
+	'''
+	n = 50
+	z = np.zeros((n, 6))
+	s = np.linspace(0, 3, num = n)
+
 	for j in range(n):
 		A0 = 1 / np.cosh(2 * T) * np.exp(1j * np.pi / 4)
-		#A0 = np.exp(-T**2 / 2)
 		A0 = np.sqrt(E0 / Energy(A0, dx)) * A0 # Normalize
 		A = A0
-		for i in range(50):
-			old = np.abs(A)
-			A = Loop(A, T, dx, s[j], b[k])
-			new = np.abs(A)
-		z[count] = s[j], b[k], np.sqrt(np.trapz((old - new)**2, dx = dx)) / np.sqrt(np.trapz(old**2, dx = dx)), Energy(A, dx)
-		count += 1
+		for i in range(25):
+			old = np.angle(A[len(A)/2])
+			A = Loop(A, T, dx, s[j], 0, switch = False)
+			new = np.angle(A[len(A)/2])
+			while old > new:
+				old -= 2 * np.pi
+		r, sigma = curve_fit(func, T, np.abs(A))[0]
+		z[j] = s[j], Energy(A, dx), np.abs(A)[len(A)/2]**2, sigma, -np.gradient(np.gradient(np.angle(A), dx), dx)[len(A)/2] * sigma**2, new - old
 
-#np.savetxt('Zeta.dat', np.vstack((s, z)).T)
-np.savetxt('a30.dat', z)
+	np.savetxt('Linear.dat', z)
 
-'''
+else:
+	print 'Please enter a valid part number (2--4)'
 
